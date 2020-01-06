6f3e9c0fb81e518606c55588ab8079f8f3449e3c
pallat <yod.pallat@gmail.com>
2020-01-06 10:24:11 +0700

    basic binding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{ 1 + 1}}
+    {{ name }}
+    <input :placeholder="placeholderName" type="text" v-model="name" />
+    <ul>
+      <li v-for="(book, index) in books" :key="index">{{ book.name }} score {{ book.score }}</li>
+    </ul>
+
+    <div v-show="toggle">Hello {{ name }}</div>
+    <div v-if="!toggle">Good morning</div>
+    <button v-on:click="clickToggle">Toggle</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  components: {},
+  methods: {
+    clickToggle() {
+      this.toggle = !this.toggle;
+    }
+  },
+  data() {
+    return {
+      name: "John",
+      placeholderName: "input your name",
+      books: [
+        { name: "Harry Potter", score: 9 },
+        { name: "Percy Jackson", score: 7 },
+        { name: "How to train the dragon", score: 5 }
+      ],
+      toggle: true
+    };
   }
 };
 </script>
